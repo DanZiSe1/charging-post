a4cb296e492c7bc9358627beca19c6135f716b62
githubzsj <zansijing@163.com>
2020-05-31 12:31:30 +0800

    firsttab

--- utils/api.js
+++ utils/api.js
@@ -3,8 +3,13 @@ var host = 'https://test-evcs.91sdc.cn';
 
 var wxBind = host + '/xcx/bind';//微信绑定
 var getOrdersList = host + '/xcx/orders';//获取订单列表
+var getChargesList = host + '/api/equip/stations';//获取附近的充电站列表
+var getStationDetail = host + '/api/equip/station/:id';//返回充电站详情
+
 
 module.exports={
   wxBind,
-  getOrdersList
+  getOrdersList,
+  getChargesList,
+  getStationDetail
 }
