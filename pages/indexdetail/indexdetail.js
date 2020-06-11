8314f074b1339a70b20bcb6e2528216bdb4ecf31
githubzsj <zansijing@163.com>
2020-06-11 22:57:56 +0800

    swiper

--- pages/indexdetail/indexdetail.js
+++ pages/indexdetail/indexdetail.js
@@ -21,7 +21,16 @@ Page({
       ServiceFee: '0.80',
       ParkFee: '0.64'
     }, */
-    resultFee: []
+    resultFee: [],
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 1500,
+    duration: 1000
   },
   onLoad: function (options) {
     // console.log(options, 'options.......')
